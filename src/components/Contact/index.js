50926071586097c633d52995d637c1538d0e90d4
HL <hi.heyse@gmail.com>
2016-09-27 00:25:18 -0400

    Fix error with embedded p's

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,10 +7,10 @@ import data from './data';
 /* eslint-disable max-len */
 const Contact = () => (
   <div className={styles.wrapper}>
-    <p className={styles.text}>
+    <div className={styles.text}>
       <p>Hi! Thanks for visiting. I'm Heyse and I like to draw, design, do math, and program things.</p>
       <p>If you would like to learn more about me and how I can help you, you can contact me at {data.email}.</p>
-    </p>
+    </div>
   </div>
 );
 
