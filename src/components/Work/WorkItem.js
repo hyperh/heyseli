30048b1b1b80f1ba00d0f1e2787f01ae33dd2ee8
HL <hi.heyse@gmail.com>
2016-09-26 21:47:57 -0400

    Load images dynamically

--- src/components/Work/WorkItem.js
+++ src/components/Work/WorkItem.js
@@ -7,14 +7,27 @@ import data from './data';
 const WorkItem = ({ params }) => {
   const findLink = find(item => item.link === params.link);
   const workItem = findLink(data);
+   // eslint-disable-next-line global-require
+  const images = workItem.images.map(image => require(`../../assets/img/${image}`));
 
   return (
     <div className={styles.wrapper}>
-      <div>{workItem.name}</div>
-      <div>{workItem.platforms}</div>
-      <div>{workItem.tech}</div>
-      <div>{workItem.url}</div>
+      <div>
+        <img src="" alt="header" />
+      </div>
+
+      <h1>{workItem.name}</h1>
+      <p>{workItem.platforms.join(', ')}</p>
       <div>{workItem.desc}</div>
+      <div>
+        <h2>Tech Used:</h2>
+        <p>{workItem.tech.join(', ')}</p>
+      </div>
+      <a href={workItem.url}>{workItem.url}</a>
+
+      <div>
+        {images.map(image => <img src={image} alt="" />)}
+      </div>
     </div>
   );
 };
--- src/components/Work/data.js
+++ src/components/Work/data.js
@@ -3,17 +3,24 @@ export default [
   {
     name: 'Texty Time',
     link: 'texty-time',
-    platform: ['Android'],
+    platforms: ['Android'],
     desc: 'An app that helps you understand your textual relationships with others.' +
     ' It computes statistics about your texting habits with other people.',
     tech: ['Java', 'Android Studio', 'Inkscape', 'Git'],
     url: 'https://play.google.com/store/apps/details?id=com.aheadstudios.talkalytics&hl=en',
-    images: [],
+    images: [
+      'texty-time/all-time.png',
+      'texty-time/compare.png',
+      'texty-time/emoticons.png',
+      'texty-time/favorites.png',
+      'texty-time/last-2-weeks.png',
+      'texty-time/main.png',
+    ],
   },
   {
     name: 'Fridgly',
     link: 'fridgly',
-    platform: ['Android'],
+    platforms: ['Android'],
     desc: 'An app that helps you keep track of your food expiration dates.' +
     ' It syncs with your Google Calendar to help remind you of when things are going to expire and when you should finish eating them.',
     tech: ['Java', 'Android Studio', 'Inkscape', 'Pixelmator'],
@@ -23,7 +30,7 @@ export default [
   {
     name: 'Driller',
     link: 'driller',
-    platform: ['Android'],
+    platforms: ['Android'],
     desc: 'An app that helps you keep track of your expenses.' +
     ' It provides detailed statistics about your spending habits to help you drill down into the areas in which you spend the most money.',
     tech: ['Java', 'Android Studio', 'Inkscape', 'Pixelmator', 'Git'],
@@ -33,7 +40,7 @@ export default [
   {
     name: 'Olis',
     link: 'olis',
-    platform: ['Web', 'PC', 'OSX', 'Android', 'iOS'],
+    platforms: ['Web', 'PC', 'OSX', 'Android', 'iOS'],
     desc: 'A business communication tool designed to help you keep the most important information visible.',
     tech: ['Javascript', 'React', 'Meteor', 'MongoDB', 'Electron', 'React Native', 'Digital Ocean', 'Sketch', 'Git'],
     url: 'https://www.olisapp.com/',
@@ -42,7 +49,7 @@ export default [
   {
     name: 'Olis Landing Page',
     link: 'olis-landing',
-    platform: ['Web'],
+    platforms: ['Web'],
     desc: 'A landing page for Olis.',
     tech: ['Javascript', 'React', 'Sketch'],
     url: 'http://www.getolis.com/',
@@ -51,7 +58,7 @@ export default [
   {
     name: 'Decide-o-Tron',
     link: 'decide-o-tron',
-    platform: ['Web'],
+    platforms: ['Web'],
     desc: 'A tool to help you make hard decisions.',
     tech: ['Javascript', 'React', 'Webpack', 'MobX', 'Sketch', 'Digital Ocean', 'Git'],
     url: 'http://decideotron.xyz',
