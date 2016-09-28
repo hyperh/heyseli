0fbf7371eb3011799185addf09281529f5ef63ca
HL <hi.heyse@gmail.com>
2016-09-28 11:18:03 -0400

    Make sidebar smaller

--- src/components/core/Sidebar.js
+++ src/components/core/Sidebar.js
@@ -6,9 +6,11 @@ import styles from './Sidebar.scss';
 const Sidebar = () => (
   <div className={styles.wrapper}>
     <div className={styles.name}>Heyse Li</div>
-    <div className={styles.item}><Link to="/contact">Contact</Link></div>
-    <div className={styles.item}><Link to="/work">Work</Link></div>
-    <div className={styles.item}><Link to="/publications">Publications</Link></div>
+    <div className={styles.links}>
+      <div className={styles.item}><Link to="/contact">Contact</Link></div>
+      <div className={styles.item}><Link to="/work">Work</Link></div>
+      <div className={styles.item}><Link to="/publications">Publications</Link></div>
+    </div>
   </div>
 );
 
