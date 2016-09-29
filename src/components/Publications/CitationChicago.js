9f501e5e02944c3b52f4060015c3559daf9862d5
HL <hi.heyse@gmail.com>
2016-09-29 16:27:38 -0400

    Bold my own name in publications

--- src/components/Publications/CitationChicago.js
+++ src/components/Publications/CitationChicago.js
@@ -1,8 +1,26 @@
 import React, { PropTypes } from 'react';
 
+// Bibtex formatted author: Last, First M and Last2, First2 M2
+const authorsWithBold = (authorStr, boldAuthor) => {
+  const authors = authorStr.split(' and ');
+  const boldIndex = authors.findIndex(author => author === boldAuthor);
+  const before = authors.slice(0, boldIndex);
+  const after = authors.slice(boldIndex + 1, authors.length);
+
+  return (
+    <span>
+      {before.join(' and ')}
+      {boldIndex !== 0 ? ' and ' : null}
+      <b>{boldAuthor}</b>
+      {boldIndex !== authors.length - 1 ? ' and ' : null}
+      {after.join(' and ')}
+    </span>
+  );
+};
+
 const CitationChicago = ({ author, title, journal, conference, number, pages, year }) => (
   <li>
-    {author}.&nbsp;
+    {authorsWithBold(author, 'Li, Heyse')}.&nbsp;
     "{title}".&nbsp;
     {journal ? <i>{journal}</i> : null}
     {number ? ` no. ${number} ` : null}
