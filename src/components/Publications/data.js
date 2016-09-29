46fee579b7d155850593446c6accce6f44ae4039
HL <hi.heyse@gmail.com>
2016-09-29 13:54:34 -0400

    Add data for publications

---
+++ src/components/Publications/data.js
@@ -0,0 +1,59 @@
+/* eslint-disable max-len */
+export default [
+  {
+    type: 'Refereed Journal Articles',
+    items: [
+      {
+        title: 'Identifying locations for public access defibrillators using mathematical optimization',
+        author: 'Chan, Timothy CY and Li, Heyse and Lebovic, Gerald and Tang, Sabrina K and Chan, Joyce YT and Cheng, Horace CK and Morrison, Laurie J and Brooks, Steven C',
+        journal: 'Circulation',
+        volume: '127',
+        number: '17',
+        pages: '1801--1809',
+        year: '2013',
+        publisher: 'Am Heart Assoc',
+      },
+      {
+        title: 'Robust PET-guided intensity-modulated radiation therapy',
+        author: 'Li, Heyse and Bissonnette, Jean-Pierre and Purdie, Tom and Chan, Timothy CY',
+        journal: 'Medical Physics',
+        volume: '42',
+        number: '8"',
+        pages: '4863-4871',
+        year: '2015',
+      },
+      {
+        title: 'The value of nodal information in predicting lung cancer relapse using 4DPET/4DCT',
+        author: 'Li, Heyse and Becker, Nathan and Raman, Srinivas and Chan, Timothy CY and Bissonnette, Jean-Pierre',
+        journal: 'Medical Physics',
+        volume: '42',
+        number: '8',
+        pages: '4727--4733',
+        year: '2015',
+        publisher: 'American Association of Physicists in Medicin',
+      },
+    ],
+  },
+  {
+    type: 'Conference Proceedings',
+    items: [
+      {
+        title: 'Resource-Aware Scheduling for Data Centers with Heterogenous Servers.',
+        author: 'Tran, Tony T and Zhang, Peter Yun and Li, Heyse and Down, Douglas G and Beck, J Christopher',
+        conference: 'Multidisciplinary International Scheduling Conference (MISTA)',
+        year: '2015',
+      },
+    ],
+  },
+  {
+    type: 'Presentations',
+    items: [
+      {
+        author: 'Li, Heyse and Bissonnette, Jean-Pierre and Purdie, Tom and Chan, Timothy CY',
+        title: 'Robust Optimization for Biologically-Guided Intensity-Modulated Radiation Therapy',
+        conference: 'INFORMS Healthcare, Chicago, IL, US',
+        year: '2013',
+      },
+    ],
+  },
+];
--- src/components/Publications/index.js
+++ src/components/Publications/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import styles from './styles.scss';
 
-// import data from './data';
+import data from './data';
 
 /* eslint-disable max-len */
 const Contact = () => (
   <div className={styles.wrapper}>
-    <p>I used to be in academia. I published a few papers during my time there.</p>
+    <p>I used to be in academia. Here are the papers I published during my time there.</p>
   </div>
 );
 
