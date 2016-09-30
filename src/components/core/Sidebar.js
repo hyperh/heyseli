cbce0e178cd6c52259ab527e378eb2b2c3eb0b22
HL <hi.heyse@gmail.com>
2016-09-30 00:55:43 -0400

    Basic burger menu working

--- src/components/core/Sidebar.js
+++ src/components/core/Sidebar.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
+import classnames from 'classnames';
 
 import styles from './Sidebar.scss';
 
-const Sidebar = () => (
-  <div className={styles.wrapper}>
-    <div className={styles.name}>Heyse Li</div>
-    <div className={styles.links}>
-      <div className={styles.item}><Link to="/contact">Contact</Link></div>
-      <div className={styles.item}><Link to="/work">Work</Link></div>
-      <div className={styles.item}><Link to="/publications">Publications</Link></div>
-    </div>
-  </div>
-);
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+    this.state = {
+      open: false, 
+    };
+  }
+
+  onClick() {
+    this.setState({ open: !this.state.open });
+  }
+
+  render() {
+    const myClass = classnames({
+      [styles.wrapper]: true,
+      [styles.open]: this.state.open,
+      [styles.closed]: !this.state.open, 
+    });
+
+    return (
+      <div>
+        <button style={{position: 'fixed'}} onClick={this.onClick}>OPEN</button>
+
+        <div className={myClass}>
+          <button onClick={this.onClick}>CLOSE</button>
+          <div className={styles.name}>Heyse Li</div>
+          <div className={styles.links}>
+            <div className={styles.item} onClick={this.onClick}><Link to="/contact">Contact</Link></div>
+            <div className={styles.item} onClick={this.onClick}><Link to="/work">Work</Link></div>
+            <div className={styles.item} onClick={this.onClick}><Link to="/publications">Publications</Link></div>
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+}
 
 export default Sidebar;
