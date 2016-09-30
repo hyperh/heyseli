a224dbf012dbed3124e44722221f8d6dc02d6273
HL <hi.heyse@gmail.com>
2016-09-29 21:13:34 -0400

    Move WorkItem header stuff to own component

--- src/components/Work/WorkItem.js
+++ src/components/Work/WorkItem.js
@@ -3,30 +3,24 @@ import find from 'lodash/fp/find';
 import { Link } from 'react-router';
 
 import styles from './WorkItem.scss';
+import WorkItemHeader from './WorkItemHeader';
 import data from './data';
 
 /* eslint-disable global-require */
-const headerImg = (workItem) => {
-  const headerImgPath = workItem.headerImg ?
-    require(`../../assets/img/${workItem.imgFolder}/${workItem.headerImg}`) : null;
-  return workItem.headerImg ?
-    <div className={styles.header}><img src={headerImgPath} alt="header" /></div> : null;
-};
-
 const WorkItem = ({ params }) => {
   const findLink = find(item => item.link === params.link);
   const workItem = findLink(data);
-
+  const { name, platforms, url, imgFolder, headerImg } = workItem;
 
   return (
     <div className={styles.wrapper}>
-      {headerImg(workItem)}
-      <div>
-        <h1>{workItem.name}</h1>
-        <p className={styles.platforms}>{workItem.platforms.join(', ')}</p>
-        <a href={workItem.url}>{workItem.url}</a>
-      </div>
-
+      <WorkItemHeader
+        name={name}
+        platforms={platforms}
+        url={url}
+        imgFolder={imgFolder}
+        headerImg={headerImg}
+      />
       <div><p>{workItem.desc}</p></div>
 
       <div>
---
+++ src/components/Work/WorkItemHeader.js
@@ -0,0 +1,32 @@
+import React, { PropTypes } from 'react';
+
+import styles from './WorkItemHeader.scss';
+
+/* eslint-disable global-require */
+const getHeaderImg = (imgFolder, headerImg) => {
+  const headerImgPath = headerImg ?
+    require(`../../assets/img/${imgFolder}/${headerImg}`) : null;
+  return headerImg ?
+    <div className={styles.header}><img src={headerImgPath} alt="header" /></div> : null;
+};
+
+const WorkItemHeader = ({ name, platforms, url, imgFolder, headerImg }) => (
+  <div className={styles.wrapper}>
+    {getHeaderImg(imgFolder, headerImg)}
+    <div>
+      <h1>{name}</h1>
+      <p className={styles.platforms}>{platforms.join(', ')}</p>
+      <a href={url}>Link</a>
+    </div>
+  </div>
+);
+
+WorkItemHeader.propTypes = {
+  name: PropTypes.string,
+  platforms: PropTypes.string,
+  url: PropTypes.string,
+  imgFolder: PropTypes.string,
+  headerImg: PropTypes.string,
+};
+
+export default WorkItemHeader;
