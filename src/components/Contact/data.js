697ad637b09f3bb7f490856bc720a2e0152af18c
HL <hi.heyse@gmail.com>
2016-09-30 11:44:40 -0400

    Add Medium and Twitter links

--- src/components/Contact/data.js
+++ src/components/Contact/data.js
@@ -1,3 +1,5 @@
 export default {
   email: 'test@test.com',
+  twitter: 'https://twitter.com/liheyse',
+  medium: 'https://medium.com/@liheyse',
 };
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,12 +5,13 @@ import styles from './styles.scss';
 import data from './data';
 
 /* eslint-disable max-len */
+const { email, twitter, medium } = data;
 const Contact = () => (
   <div className={styles.wrapper}>
     <p>Hi! Thanks for visiting. I'm Heyse and I like to draw, design, do math, and program things.</p>
-    <p>If you would like to learn more about me and how I can help, you can contact me at {data.email}.</p>
+    <p>If you would like to learn more about me and how I can help, you can contact me at {email}.</p>
     <p>
-      You can also follow me on Twitter and Medium.
+      You can also follow me on <a href={twitter}>Twitter</a> and <a href={medium}>Medium</a>.
       I write about things on Medium from time to time,
       mostly about issues that I encounter when programming and how I solved it.
     </p>
