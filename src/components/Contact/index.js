056f15387c4fa531b2b39956b451c8b1c1bd3c80
HL <hi.heyse@gmail.com>
2016-10-12 15:37:12 -0400

    Add github link to contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,15 +6,17 @@ import data from './data';
 import HiddenEmail from './HiddenEmail';
 
 /* eslint-disable max-len */
-const { email, twitter, medium } = data;
+const { email, twitter, medium, github } = data;
 const Contact = () => (
   <div className={styles.wrapper}>
     <p>Hi! Thanks for visiting. I'm Heyse and I like to draw, design, do math, and program things.</p>
     <p>If you would like to learn more about me and how I can help, you can contact me at <b><i><HiddenEmail email={email} /></i></b>.</p>
     <p>
       You can also follow me on <a href={twitter}>Twitter</a> and <a href={medium}>Medium</a>.
-      I write about things on Medium from time to time,
-      mostly about issues that I encounter when programming and how I solved it.
+      I write about things on Medium from time to time, mostly about issues that I encounter when programming and how I solved it.
+    </p>
+    <p>
+      I also have a <a href={github}>Github</a> account, where you can view samples of my code and the contributions I have made to the open source community.
     </p>
   </div>
 );
