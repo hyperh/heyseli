803c0fded8b29d1944d3652b9d3b793e45286d94
HL <hi.heyse@gmail.com>
2016-12-25 13:39:25 -0500

    Update resume

--- src/components/Resume/data.js
+++ src/components/Resume/data.js
@@ -2,33 +2,38 @@ export default {
   experience: [
     {
       title: 'Full Stack Developer',
+      company: 'Brain.fm',
+      time: '2016 Nov - Present',
+    },
+    {
+      title: 'Full Stack Developer',
       company: 'Branch8',
-      time: '2016',
+      time: '2016 Jun - Aug',
     },
     {
       title: 'Full Stack Developer, Designer',
       company: 'Olis',
-      time: '2016',
+      time: '2015 Nov - Jun',
     },
     {
       title: 'Programming Teacher',
       company: 'Capstone Prep',
-      time: '2016',
+      time: '2016 Jun - Aug',
     },
     {
       title: 'Research Associate',
       company: 'University of Toronto - Applied Optimization Lab',
-      time: '2011-2015',
+      time: '2011 - 2015',
     },
     {
       title: 'Research Associate',
       company: 'Princess Margaret Cancer Center - Radiation Physics',
-      time: '2013-2015',
+      time: '2013 - 2015',
     },
     {
       title: 'Research Assistant',
       company: 'University of Toronto - Toronto Intelligent Decision Engineering Lab',
-      time: '2011',
+      time: '2011 Jun - Sept',
     },
     {
       title: 'Private Tutor',
@@ -38,7 +43,7 @@ export default {
     {
       title: 'Business Analyst',
       company: 'Canadian Imperial Bank of Commerce - Process Optimization',
-      time: '2009-2010',
+      time: '2009 Jun - 2010 Sep',
     },
   ],
   education: [
