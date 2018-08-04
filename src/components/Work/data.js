89a6602c0b0b589ce8afb9eccba6bde987ba9214
HL <>
2018-08-04 01:18:53 -0400

    Add bZx and related assets

--- src/components/Work/data.js
+++ src/components/Work/data.js
@@ -355,5 +355,33 @@ export default [
       'drawer.png'
     ],
     year: 2017
+  },
+  {
+    name: 'bZx',
+    link: 'bzx',
+    platforms: ['Web'],
+    desc: (
+      <div>
+        <p>
+          A decentralized margin trading cryptocurrency exchange for ERC20
+          tokens.
+        </p>
+
+        <p>
+          I was responsible for writing the Javascript library for dApp
+          developers to integrate bZx into their applications. I worked in a
+          team of three to successfully integrate the library with the bZx
+          portal and bzX smart contracts and deploy onto the Ropsten and Kovan
+          testnets.
+        </p>
+      </div>
+    ),
+    tech: ['Javascript', 'React', 'Solidity', 'Jest', 'Git'],
+    url: 'https://bzx.network/',
+    imgFolder: 'bzx',
+    previewImg: 'logo.png',
+    headerImg: 'logo.png',
+    images: ['gen_order_screen.png', 'balances_screen.png'],
+    year: 2017
   }
 ];
