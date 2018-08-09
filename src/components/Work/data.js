19511bd799a3cf6fb4c393b857fc5df0dc1254e8
HL <>
2018-08-09 14:44:23 -0400

    Add data center simulation to work

--- src/components/Work/data.js
+++ src/components/Work/data.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import simPaper from '../../assets/papers/sim.pdf';
 
 const bfmTechLead = (
   <p>
@@ -12,6 +13,28 @@ const bfmTechLead = (
 
 /* eslint-disable max-len */
 export default [
+  {
+    name: 'Data Center Simulation',
+    link: 'data-center',
+    platforms: ['Desktop'],
+    desc: (
+      <div>
+        <p>
+          A platform to run simulations to aid in the research of resource-aware
+          scheduling of computational jobs in a large-scale heterogeneous data
+          center. The project was funded by a Google Research Award and the
+          Natural Sciences and Engineering Research Council of Canada (NSERC).
+        </p>
+        <p>
+          I worked in a team of 3 to develop the simulation. The research
+          eventually yielded a{' '}
+          <a href={simPaper}>refereed conference article in MISTA</a>.
+        </p>
+      </div>
+    ),
+    tech: ['C++', 'SVN'],
+    year: 2011
+  },
   {
     name: 'Texty Time',
     link: 'texty-time',
