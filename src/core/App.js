c579af930a2c31527c065c2be969231d5d9fd08e
HL <>
2018-11-15 18:04:35 +0900

    Refactor

--- src/core/App.js
+++ src/core/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import styles from './App.scss';
 import Sidebar from './Sidebar';
 import Content from './Content';
@@ -9,16 +9,18 @@ import Publications from '../publications/Publications';
 import Work from '../work/Work';
 
 const App = () => (
-  <div className={styles.app}>
-    <Sidebar />
-    <Content>
-      <Route path="/" component={Contact} exact />
-      <Route path="/contact" component={Contact} exact />
-      <Route path="/resume" component={Resume} exact />
-      <Route path="/publications" component={Publications} exact />
-      <Route path="/work" component={Work} exact />
-    </Content>
-  </div>
+  <Router>
+    <div className={styles.app}>
+      <Sidebar />
+      <Content>
+        <Route path="/" component={Contact} exact />
+        <Route path="/contact" component={Contact} exact />
+        <Route path="/resume" component={Resume} exact />
+        <Route path="/publications" component={Publications} exact />
+        <Route path="/work" component={Work} exact />
+      </Content>
+    </div>
+  </Router>
 );
 
 export default App;
