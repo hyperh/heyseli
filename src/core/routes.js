d6434825ac7c98c1b6493d843f1d1d706a3e9685
HL <>
2018-11-15 17:22:41 +0900

    Run react router dom successfully

--- src/core/routes.js
+++ src/core/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import App from './App';
+// import App from './App';
 // import Work from '../work/Work';
 // import WorkItem from '../work/WorkItem';
 // import Contact from '../contact/Contact';
@@ -11,15 +11,13 @@ const MyTest = () => <div>hello</div>;
 
 const Routes = () => (
   <Router>
-    <Route path="/" component={App}>
-      <Route component={MyTest} />
-      {/* <IndexRoute component={Contact} />
+    <Route component={MyTest} />
+    {/* <IndexRoute component={Contact} />
       <Route path="work" component={Work} />
       <Route path="work/:link" component={WorkItem} />
       <Route path="contact" component={Contact} />
       <Route path="publications" component={Publications} />
       <Route path="resume" component={Resume} /> */}
-    </Route>
   </Router>
 );
 
