d84d2f2720a82938484ba012954ad60e25e36d41
HL <>
2018-11-15 17:50:22 +0900

    Get resume to render

--- src/core/App.js
+++ src/core/App.js
@@ -4,13 +4,15 @@ import styles from './App.scss';
 import Sidebar from './Sidebar';
 import Content from './Content';
 import Contact from '../contact/Contact';
+import Resume from '../resume/Resume';
 
 const App = () => (
   <div className={styles.app}>
     <Sidebar />
     <Content>
-      <Route path="/" component={Contact} />
-      <Route path="/contact" component={Contact} />
+      <Route path="/" component={Contact} exact />
+      <Route path="/contact" component={Contact} exact />
+      <Route path="/resume" component={Resume} exact />
     </Content>
   </div>
 );
--- src/core/routes.js
+++ src/core/routes.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 // import WorkItem from '../work/WorkItem';
 // import Publications from '../publications/Publications';
-// import Resume from '../resume/Resume';
 
 const Routes = () => (
   <Router>
@@ -13,7 +12,7 @@ const Routes = () => (
       <Route path="work/:link" component={WorkItem} />
       
       <Route path="publications" component={Publications} />
-      <Route path="resume" component={Resume} /> */}
+       */}
   </Router>
 );
 
