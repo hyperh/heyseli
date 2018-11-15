3b62a364c540d8ba8e62877dcf9632017be90a06
HL <>
2018-11-15 19:16:17 +0900

    Use HashRouter since this is all just JS and no server or different html files

--- src/core/App.js
+++ src/core/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import styles from './App.scss';
 import Sidebar from './Sidebar';
 import Content from './Content';
