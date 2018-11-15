5cffeab58a558bdda80603bc317c3ebbcff23c2a
HL <>
2018-11-15 17:09:19 +0900

    Fix proptypes

--- src/work/WorkItem.js
+++ src/work/WorkItem.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import find from 'lodash/fp/find';
 
 import styles from './WorkItem.scss';
