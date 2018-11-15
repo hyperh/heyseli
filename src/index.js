51b687dcf80601d432214facab9b09896db51dee
HL <>
2018-11-15 16:16:49 +0900

    Upgrade loaders Strip webpack config to bare min Successfully run dev server

--- src/index.js
+++ src/index.js
@@ -1,23 +1,4 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import Routes from './core/routes';
+import ReactDOM from 'react-dom';
 
-render(
-  <AppContainer>
-    <Routes />
-  </AppContainer>,
-  document.getElementById('root')
-);
-
-if (module.hot) {
-  module.hot.accept('./core/routes', () => {
-    const NextRoutes = require('./core/routes').default; // eslint-disable-line global-require
-    render(
-      <AppContainer>
-        <NextRoutes />
-      </AppContainer>,
-      document.getElementById('root')
-    );
-  });
-}
+ReactDOM.render(<div>Testing</div>, document.getElementById('root'));
