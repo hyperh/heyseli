94cd8cfa87d99e524d6f9552e29b2bf440be534a
HL <>
2018-11-15 15:13:44 +0900

    Use webpack 4 syntax Remove deprecated plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,100 +1,99 @@
 const path = require('path');
 const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
+// const autoprefixer = require('autoprefixer');
 
 const isProd = process.env.NODE_ENV === 'production';
 
-const devPlugins = [
-  new webpack.HotModuleReplacementPlugin(),
-];
+const devPlugins = [new webpack.HotModuleReplacementPlugin()];
 const prodPlugins = [
-  new webpack.optimize.DedupePlugin(),
-  new webpack.optimize.OccurenceOrderPlugin(),
+  // new webpack.optimize.DedupePlugin(),
+  // new webpack.optimize.OccurenceOrderPlugin(),
   new webpack.DefinePlugin({
     'process.env': {
-      NODE_ENV: JSON.stringify('production'),
-    },
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    screw_ie8: true,
-    compressor: { warnings: false },
-  }),
+      NODE_ENV: JSON.stringify('production')
+    }
+  })
+  // new webpack.optimize.UglifyJsPlugin({
+  //   screw_ie8: true,
+  //   compressor: { warnings: false }
+  // })
 ];
 
 /* eslint-disable max-len */
 module.exports = {
+  mode: 'development',
   devtool: 'eval',
-  entry: isProd ?
-    './src/index' :
-    [
-      `webpack-dev-server/client?http://localhost:${process.env.PORT}`,
-      'webpack/hot/only-dev-server',
-      'react-hot-loader/patch',
-      './src/index',
-    ],
+  entry: isProd
+    ? './src/index'
+    : [
+        `webpack-dev-server/client?http://localhost:${process.env.PORT}`,
+        'webpack/hot/only-dev-server',
+        'react-hot-loader/patch',
+        './src/index'
+      ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '',
+    publicPath: ''
   },
   plugins: isProd ? prodPlugins : devPlugins,
   module: {
-    preLoaders: [
-      {
-        test: /\.js/,
-        loader: 'eslint',
-      },
-    ],
-    loaders: [
+    // preLoaders: [
+    //   {
+    //     test: /\.js/,
+    //     loader: 'eslint'
+    //   }
+    // ],
+    rules: [
       {
         test: /\.js$/,
-        loaders: ['babel'],
-        include: path.join(__dirname, 'src'),
+        use: ['babel-loader'],
+        include: path.join(__dirname, 'src')
       },
       {
         test: /\.css/,
-        loaders: [
-          'style',
-          'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-          'postcss',
+        use: [
+          'style-loader',
+          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+          'postcss-loader'
         ],
         include: [
           path.join(__dirname, 'src'),
-          path.join(__dirname, 'node_modules/react-photoswipe/lib'),
-        ],
+          path.join(__dirname, 'node_modules/react-photoswipe/lib')
+        ]
       },
       {
         test: /\.scss$/,
-        loaders: [
-          'style',
-          'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-          'resolve-url',
-          'sass?sourceMap',
+        use: [
+          'style-loader',
+          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+          'resolve-url-loader',
+          'sass-loader?sourceMap'
         ],
-        include: path.join(__dirname, 'src'),
+        include: path.join(__dirname, 'src')
       },
       {
         test: /\.(png|jpg|svg|gif)/,
-        loaders: [
-          'url?limit=10000&hash=sha512&digest=hex&name=public/img/[name]_[hash].[ext]',
-          'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "50-75", speed: 4}}',
+        use: [
+          'url-loader?limit=10000&hash=sha512&digest=hex&name=public/img/[name]_[hash].[ext]',
+          'image-webpack-loader?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "50-75", speed: 4}}'
         ],
         include: [
           path.join(__dirname, 'src'),
-          path.join(__dirname, 'node_modules/react-photoswipe/lib'),
-        ],
+          path.join(__dirname, 'node_modules/react-photoswipe/lib')
+        ]
       },
       {
         test: /\.(eot|ttf|woff|woff2|otf)$/,
-        loader: 'file?name=public/fonts/[name].[ext]',
-        include: path.join(__dirname, 'src'),
+        use: 'file-loader?name=public/fonts/[name].[ext]',
+        include: path.join(__dirname, 'src')
       },
       {
         test: /\.(pdf)/,
-        loader: 'file?name=public/papers/[name].[ext]',
-        include: path.join(__dirname, 'src'),
-      },
-    ],
-  },
-  postcss: () => [autoprefixer],
+        use: 'file-loader?name=public/papers/[name].[ext]',
+        include: path.join(__dirname, 'src')
+      }
+    ]
+  }
+  // postcss: () => [autoprefixer]
 };
