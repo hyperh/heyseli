00ba9eccd2c8704c77464965220453ce7f761525
HL <>
2019-06-19 15:40:14 -0400

    Add ganache logo and tech

---
+++ src/work/ganache/data.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import imgLogo from './logo.png';
+
+export default {
+  name: 'Ganache',
+  link: 'ganache',
+  platforms: ['Electron'],
+  desc: <p>tbd</p>,
+  tech: ['Javascript', 'Electron', 'React', 'Redux', 'Redux Thunks', 'Jest'],
+  imgFolder: 'ganache',
+  previewImg: imgLogo,
+  headerImg: imgLogo,
+  year: 2018.12,
+  url: 'https://www.trufflesuite.com/ganache'
+};
