d9c94ea1a3c5931997b357d044007b0fc5ab0081
HL <>
2019-06-19 16:02:03 -0400

    Add Truffle Teams logo, tech, url

---
+++ src/work/truffle-teams/data.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import imgLogo from './logo.svg';
+
+export default {
+  name: 'Truffle Teams',
+  link: 'truffle-teams',
+  platforms: ['Web'],
+  desc: <p>tbd</p>,
+  tech: [
+    'Javascript',
+    'React',
+    'React Hooks',
+    'Redux',
+    'Redux Thunks',
+    'Apollo',
+    'GraphQL',
+    'Express',
+    'Storybook'
+  ],
+  imgFolder: 'truffle-teams',
+  previewImg: imgLogo,
+  headerImg: imgLogo,
+  year: 2019.3,
+  url: 'https://www.trufflesuite.com/teams'
+};
