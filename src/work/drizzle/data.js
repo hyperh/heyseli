f6efdef9aa7a9b15ad2a01db105b23294827203a
HL <>
2019-06-19 15:54:21 -0400

    Add Drizzle logo, tech, urls

---
+++ src/work/drizzle/data.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import imgLogo from './logo.png';
+
+export default {
+  name: 'Drizzle',
+  link: 'drizzle',
+  platforms: ['Web', 'React Native'],
+  desc: <p>tbd</p>,
+  tech: ['Javascript', 'React Native', 'React', 'Redux', 'Redux Sagas', 'RxJS'],
+  imgFolder: 'drizzle',
+  previewImg: imgLogo,
+  headerImg: imgLogo,
+  year: 2018.12,
+  url: [
+    { name: 'Website', value: 'https://www.trufflesuite.com/drizzle' },
+    {
+      name: 'Github: Drizzle',
+      value: 'https://github.com/trufflesuite/drizzle'
+    },
+    {
+      name: 'Github: Drizzle React Components',
+      value: 'https://github.com/trufflesuite/drizzle-react-components'
+    },
+    {
+      name: 'Github: Drizzle Utils',
+      value: 'https://github.com/trufflesuite/drizzle-utils'
+    }
+  ]
+};
