9267259a7458d5760f05cdd8fd738e70d89a77b9
HL <>
2019-10-07 21:22:02 -0400

    Update resume

--- src/resume/data.js
+++ src/resume/data.js
@@ -2,8 +2,13 @@ export default {
   experience: [
     {
       title: 'Blockchain Tools Engineer, Software Engineer - Front End',
+      company: 'Truffle Blockchain Group',
+      time: '2019 Mar - present'
+    },
+    {
+      title: 'Blockchain Tools Engineer, Software Engineer - Front End',
       company: 'Consensys - Truffle',
-      time: '2018 Dec - present'
+      time: '2018 Dec - 2019 Mar'
     },
     {
       title: 'Full Stack Developer',
