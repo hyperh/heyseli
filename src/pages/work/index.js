dd25ea8ca874bfb6a715d7061acbcfeb0cb07696
HL <hyperh@users.noreply.github.com>
2020-05-17 01:26:44 -0400

    Add work page

---
+++ src/pages/work/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Work from '../../work/Work';
+import Page from '../../core/Page';
+
+const WorkPage = () => (
+  <Page>
+    <Work />
+  </Page>
+);
+
+export default WorkPage;
