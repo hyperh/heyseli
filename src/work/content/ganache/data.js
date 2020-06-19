cf94a59cad956438831e34b9f1948013a46469c9
HL <hyperh@users.noreply.github.com>
2020-06-19 14:04:59 -0400

    Update ganache text

--- src/work/content/ganache/data.js
+++ src/work/content/ganache/data.js
@@ -8,7 +8,9 @@ export default {
   desc: (
     <p>
       A local blockchain and blockchain explorer for developers. I worked in a
-      team of 3, fixed bugs, reviewed PRs
+      team of 3+. I was responsible for fixing bugs and reviewing PRs. I fixed a
+      critical memory bug with the application that was causing it to crash if
+      left open too long.
     </p>
   ),
   tech: [
