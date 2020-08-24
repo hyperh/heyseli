a3620348ab22400c82cd40f942d95646f0e372fd
HL <hyperh@users.noreply.github.com>
2020-08-23 20:01:27 -0400

    Move colors to theme file

--- src/core/Page.js
+++ src/core/Page.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
 import Sidebar from './Sidebar';
 import Content from './Content';
 import { headerFont } from './fonts';
+import useDarkMode from 'use-dark-mode';
+import theme from './theme';
 
 const GlobalStyle = createGlobalStyle`
   html,
@@ -14,6 +16,21 @@ const GlobalStyle = createGlobalStyle`
     width: 100%;
     color: rgba(0, 0, 0, 87);
     font-weight: 300;
+  }
+
+  body.light-mode {
+    background-color: #fff;
+    color: #333;
+    transition: all 0.3s ease;
+  }
+  body.dark-mode {
+    background-color: #1a1919;
+    color: #999;
+    transition: all 0.3s ease;
+  }
+
+  a:visited {
+    color: green;
   }
 
   /* To make sure padding doesn't change size of elements */
@@ -36,15 +53,22 @@ const Wrapper = styled.div`
   justify-content: center;
 `;
 
-const Page = ({ children }) => (
-  <>
-    <GlobalStyle />
-    <Wrapper>
-      <Sidebar />
-      <Content>{children}</Content>
-    </Wrapper>
-  </>
-);
+const Page = ({ children }) => {
+  const darkMode = useDarkMode(false);
+
+  return (
+    <ThemeProvider theme={darkMode.value ? theme.dark : theme.light}>
+      <GlobalStyle />
+      <Wrapper>
+        <div>
+          <Sidebar />
+          <button onClick={darkMode.toggle}>Toggle</button>
+        </div>
+        <Content>{children}</Content>
+      </Wrapper>
+    </ThemeProvider>
+  );
+};
 
 Page.propTypes = {
   children: PropTypes.node,
---
+++ src/core/theme.js
@@ -0,0 +1,10 @@
+const theme = {
+  light: {
+    secondaryText: 'rgba(0, 0, 0, 0.54)',
+  },
+  dark: {
+    secondaryText: 'white',
+  },
+};
+
+export default theme;
